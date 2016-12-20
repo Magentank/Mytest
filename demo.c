3de6199f8f1a1ed28d89369bd24ebf291a109639
Magentank <magentamaschine@gmail.com>
2016-12-20 03:39:55 -0500

    First Commit

---
+++ demo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/* demo.c My first C program on a Linux */
+
+int main(void)
+{
+  printf("Hello! This is a test program.\n"
+  "Had this been a REAL program, \n"
+  "something would have happened. \n"
+  "This is ONLY a test.\n");
+  return 0;
+}
